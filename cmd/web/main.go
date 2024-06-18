9f56de1342feffec3c4283a5ffe762312dd70076
Punnarat Rattanapawan <punnarat_mos@hotmail.com>
2024-06-18 14:04:49 +0700

    feat: ch3.3 dependency injection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,11 +7,19 @@ import (
 	"os"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		logger: logger,
+	}
 
 	mux := http.NewServeMux()
 
@@ -22,10 +30,10 @@ func main() {
 	// it is known as a 'subtree path pattern', they act like "/**" or "/static/**"
 	//
 	// To prevent subtree path pattern, append `{$}` -- like "/{$}" or "/static/{$}"
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
 	logger.Info("starting server", "addr", *addr)
 
