98c36e0be68a870b2bf75532cb0bf79103ab548a
Punnarat Rattanapawan <punnarat_mos@hotmail.com>
2024-06-18 16:28:58 +0700

    feat: ch.3.5 isolating the application routes

---
+++ cmd/web/routes.go
@@ -0,0 +1,21 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	// Route pattern that ends with trailing slash -- like "/" or "/static/" --
+	// it is known as a 'subtree path pattern', they act like "/**" or "/static/**"
+	//
+	// To prevent subtree path pattern, append `{$}` -- like "/{$}" or "/static/{$}"
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+
+	return mux
+}
