f9d9c8a5f3916f116d223ba594229aa80b380153
Punnarat Rattanapawan <puunarat_mos@hotmail.com>
2024-06-27 00:48:04 +0700

    feat: ch5.3 caching templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,10 +1,54 @@
 package main
 
-import "snippetbox.mzwallow.dev/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"snippetbox.mzwallow.dev/internal/models"
+)
 
 // Define a templateData type to act as the holding structure for
 // any dynamic data that we want to pass to our HTML templates.
 type templateData struct {
 	Snippet  models.Snippet
 	Snippets []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	// Use the filepath.Glob() function to get a slice of all filepaths that
+	// match the pattern "./ui/html/pages/*.tmpl". This will essentially gives
+	// us a slice of all the filepaths for our application 'page' templates
+	// like: [ui/html/pages/home.tmpl ui/html/pages/view.tmpl]
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		// Extract the file name (like 'home.tmpl') from the full filepath.
+		name := filepath.Base(page)
+
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseGlob() *on this template set* to add any partials.
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseFiles() *on this template set* to add the  page template.
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
