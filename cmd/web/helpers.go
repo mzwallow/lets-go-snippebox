677b3bab7032a76ddcb3c7f452f573bb621d28a0
Punnarat Rattanapawan <puunarat_mos@hotmail.com>
2024-06-27 00:58:54 +0700

    feat: ch5.5 common dynamic data

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime/debug"
+	"time"
 )
 
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
@@ -54,4 +55,10 @@ func (app *application) render(
 	w.WriteHeader(status)
 
 	buf.WriteTo(w)
+}
+
+func (app *application) newTemplateData(r *http.Request) templateData {
+	return templateData{
+		CurrentYear: time.Now().Year(),
+	}
 }
