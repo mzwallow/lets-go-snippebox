e8bf63aa2e79bf2f8aa4c45d698e3f7e3d907126
Punnarat Rattanapawan <puunarat_mos@hotmail.com>
2024-06-26 23:46:05 +0700

    feat: ch5.2 template actions and functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,8 +4,7 @@ import "snippetbox.mzwallow.dev/internal/models"
 
 // Define a templateData type to act as the holding structure for
 // any dynamic data that we want to pass to our HTML templates.
-// At the moment it only contains one field, but we'll add more
-// to it as the build progresses.
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }
