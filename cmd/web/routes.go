5772115fe874fc44cb7a8dbd17513fa8f11ed453
Punnarat Rattanapawan <punnarat_mos@hotmail.com>
2024-06-30 20:06:58 +0700

    feat: ch8.2 setting up the session manager

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,12 +16,14 @@ func (app *application) routes() http.Handler {
 	// it is known as a 'subtree path pattern', they act like "/**" or "/static/**"
 	//
 	// To prevent subtree path pattern, append `{$}` -- like "/{$}" or "/static/{$}"
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
+
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
+	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
+	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
-
 	return standard.Then(mux)
 }
