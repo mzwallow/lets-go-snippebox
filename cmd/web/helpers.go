f75ef36e2272db46201b572ee759f3d64beaee9b
Punnarat Rattanapawan <puunarat_mos@hotmail.com>
2024-07-25 00:00:24 +0700

    feat: ch10.6 user authorization

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -62,8 +62,9 @@ func (app *application) render(
 
 func (app *application) newTemplateData(r *http.Request) templateData {
 	return templateData{
-		CurrentYear: time.Now().Year(),
-		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+		CurrentYear:     time.Now().Year(),
+		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
+		IsAuthenticated: app.isAuthenticated(r),
 	}
 }
 
@@ -85,4 +86,8 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 	}
 
 	return nil
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
 }
