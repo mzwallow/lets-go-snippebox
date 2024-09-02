e5856a2ac50aed1689d1dc656d7aecf992e27a70
Punnarat Rattanapawan <punnarat_mos@hotmail.com>
2024-09-02 23:31:24 +0700

    feat: ch12.1 embedding static files

---
+++ ui/efs.go
@@ -0,0 +1,18 @@
+package ui
+
+import "embed"
+
+//go:embed "static"
+var Files embed.FS
+
+// To embed multiple paths,
+// "static/css" "static/img" "static/js"
+// Only use forward slash (/) as a path sperator.
+//
+// Can also embed specific files.
+// Can use wildcard (*).
+//
+// Finally, if a path is to a directory, then all files in that directory
+// are recursively embedded — except for files with names that begin
+// with . or _ characters. If you want to include those files too, then you
+// should use the all: prefix at the start of the path.
